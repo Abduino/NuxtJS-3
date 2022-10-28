05f2ba7c385900c703becc954c922be2d85ba204
Abduino <abdrehmanredi0@gmail.com>
2022-10-28 17:30:10 -0300

    initialize nuxt project

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script setup>
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+</script>
 <template>
   <div>
-    <h1>abdurehman</h1>
+    Counter: {{ counter }}
+    <button @click="counter++">
+      Increment
+    </button>
+    <button @click="counter--">
+      Decrement
+    </button>
   </div>
 </template>
